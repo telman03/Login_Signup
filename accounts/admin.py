d44b7516ad370f05105ea5e38209ec676caa8c85
telman03 <telmangadimov1@gmail.com>
2021-11-29 20:15:38 +0400

    initial commit

--- accounts/admin.py
+++ accounts/admin.py
@@ -10,13 +10,6 @@ class CustomUserAdmin(UserAdmin):
     form = CustomUserChangeForm
     model = CustomUser
     list_display = ['email', 'username', 'is_staff']
-    # fieldsets = UserAdmin.fieldsets + (
-    #     (None, {'fields': ('age',)}),
-    # )
-    #
-    # add_fieldsets = UserAdmin.add_fieldsets + (
-    #     (None, {'fields': ('age',)}),
-    # )
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
