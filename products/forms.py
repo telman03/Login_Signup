d90b068a27f965a0b80753e0574eb75a988e2d47
telman03 <telmangadimov1@gmail.com>
2021-12-03 12:13:06 +0400

    added delete and edit view

--- products/forms.py
+++ products/forms.py
@@ -1,23 +1,23 @@
-# -*- coding: utf-8 -*-
-from django import forms
+# # -*- coding: utf-8 -*-
+# from django import forms
 
-from .models import Product
+# from .models import Product
 
-class ProductForm(forms.ModelForm):
-    def __init__(self, *args, **kwargs):
-        super(ProductForm, self).__init__(*args, **kwargs)
-        self.fields['name'].widget.attrs = {
-            'class': 'form-control col-md-6'
-        }
-        self.fields['description'].widget.attrs = {
-            'class': 'form-control col-md-6'
-        }
-        self.fields['price'].widget.attrs = {
-            'class': 'form-control col-md-6',
-            'step': 'any',
-            'min': '1',
-        }
+# class ProductForm(forms.ModelForm):
+#     # def __init__(self, *args, **kwargs):
+#     #     super(ProductForm, self).__init__(*args, **kwargs)
+#     #     self.fields['name'].widget.attrs = {
+#     #         'class': 'form-control col-md-6'
+#     #     }
+#     #     self.fields['description'].widget.attrs = {
+#     #         'class': 'form-control col-md-6'
+#     #     }
+#     #     self.fields['price'].widget.attrs = {
+#     #         'class': 'form-control col-md-6',
+#     #         'step': 'any',
+#     #         'min': '1',
+#     #     }
 
-    class Meta:
-        model = Product
-        fields = ('name', 'description', 'price')
+#     class Meta:
+#         model = Product
+#         fields = ('name', 'description', 'price')
